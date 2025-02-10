ac77f5168ce077c83f6afd88388f49c946b57e92
Skippy <steelstone3@gmail.com>
2025-02-10 07:43:01 +0000

    version-10-02-25

--- lint.sh
+++
@@ -1,4 +0,0 @@
-cargo clippy -- -D clippy::unwrap_used
-cargo clippy -- -D clippy::expect_used
-cargo clippy -- -D clippy::panic
-cargo clippy -- -D unused_must_use
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ mod controllers;
 mod models;
 mod views;
 
+#[deny(clippy::unwrap_used)]
+#[deny(clippy::expect_used)]
+#[deny(clippy::panic)]
+#[deny(unused_must_use)]
 pub fn main() -> iced::Result {
     iced::application("Karting Time", KartingTime::update, KartingTime::view)
         .theme(KartingTime::theme)
