de606db6d4a743875a75a42add4fd5f559f76a5f
Skippy <steelstone3@gmail.com>
2025-04-23 01:31:04 +0100

    version-23-04-25

--- src/views/application/search_bar.rs
+++ src/views/application/search_bar.rs
@@ -1,5 +1,5 @@
 use crate::{commands::messages::Message, models::application::karting_time::KartingTime};
-use iced::widget::{column, row, text, text_input};
+use iced::widget::{column, text, text_input};
 
 impl KartingTime {
     pub fn search_bar_view(&self) -> iced::widget::Column<Message> {
@@ -7,19 +7,21 @@ impl KartingTime {
             .push(text("Filter").size(24))
             .padding(10)
             .spacing(10)
-            .push(
-                row!().push(text("Track:")).push(
-                    text_input("Enter track filter", &self.application_state.track_query)
-                        .on_input(Message::TrackFilterChanged),
-                ),
-            )
+            .push(text("Track:"))
             .padding(10)
             .spacing(10)
             .push(
-                row!().push(text("Date:")).push(
-                    text_input("Enter date filter", &self.application_state.date_query)
-                        .on_input(Message::DateFilterChanged),
-                ),
+                text_input("Enter track filter", &self.application_state.track_query)
+                    .on_input(Message::TrackFilterChanged),
+            )
+            .padding(10)
+            .spacing(10)
+            .push(text("Date:"))
+            .padding(10)
+            .spacing(10)
+            .push(
+                text_input("Enter date filter", &self.application_state.date_query)
+                    .on_input(Message::DateFilterChanged),
             )
             .padding(10)
             .spacing(10)
