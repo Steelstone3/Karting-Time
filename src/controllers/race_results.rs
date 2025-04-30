faaf4678e285a5791feed35614fd1de69831c966
Skippy <steelstone3@gmail.com>
2025-04-30 03:19:16 +0100

    update

--- src/controllers/race_results.rs
+++ src/controllers/race_results.rs
@@ -1,4 +1,5 @@
 pub mod calculate_race_time_summaries;
 pub mod display_race_results;
-pub mod laptime_converter;
 pub mod display_race_summary;
+pub mod laptime_converter;
+pub mod update_existing_race;
--- src/models/driver/race_result.rs
+++ src/models/driver/race_result.rs
@@ -16,34 +16,6 @@ impl Race {
         }
     }
 
-    pub fn is_unique_identifer(&self, races: &Vec<Race>) -> bool {
-        for race in races {
-            if RaceInformation::get_unique_race_identifier(&self.race_information)
-                == RaceInformation::get_unique_race_identifier(&race.race_information)
-                || self.race_information.track_name.is_empty()
-            {
-                return false;
-            }
-        }
-
-        true
-    }
-
-    pub fn replace_existing_race(&self, races: &[Race]) -> Vec<Race> {
-        let mut updated_races = races.to_owned();
-
-        for i in 0..updated_races.len() {
-            if RaceInformation::get_unique_race_identifier(&self.race_information)
-                == RaceInformation::get_unique_race_identifier(&updated_races[i].race_information)
-            {
-                updated_races[i] = self.clone();
-                return updated_races;
-            }
-        }
-
-        updated_races
-    }
-
     fn convert_laps_to_laptimes(&self) -> Vec<String> {
         let mut laps: Vec<String> = vec![];
 
@@ -61,7 +33,6 @@ impl Race {
 mod race_result_should {
     use super::*;
     use crate::models::date::Date;
-    use rstest::rstest;
 
     #[test]
     fn convert_to_race_file() {
@@ -111,121 +82,7 @@ mod race_result_should {
         assert_eq!(expected_race_file, race_file);
     }
 
-    #[rstest]
-    #[case(Race{ race_information: RaceInformation { track_name: "Three Sisters".to_string(), date: Date{ day: 15, month: 11, year: 2025 }, session_id: 1, race_position: 1 }, ..Default::default() },Race{ race_information: RaceInformation { track_name: "Three Sisters".to_string(), date: Date{ day: 15, month: 11, year: 2025 }, session_id: 1, race_position: 1 }, ..Default::default() },Race{ race_information: RaceInformation { track_name: "Three Sisters".to_string(), date: Date{ day: 15, month: 11, year: 2025 }, session_id: 1, race_position: 1 }, ..Default::default() }, false)]
-    #[case(Race{ race_information: RaceInformation { track_name: "Three Sisters".to_string(), date: Date{ day: 15, month: 11, year: 2025 }, session_id: 1, race_position: 1 }, ..Default::default() },Race{ race_information: RaceInformation { track_name: "Three Sisters".to_string(), date: Date{ day: 16, month: 11, year: 2025 }, session_id: 1, race_position: 1 }, ..Default::default() },Race{ race_information: RaceInformation { track_name: "Three Sisters".to_string(), date: Date{ day: 17, month: 11, year: 2025 }, session_id: 1, race_position: 1 }, ..Default::default() }, true)]
-    #[case(Race{ race_information: RaceInformation { track_name: "Three Sisters".to_string(), date: Date{ day: 15, month: 11, year: 2025 }, session_id: 1, race_position: 1 }, ..Default::default() },Race{ race_information: RaceInformation { track_name: "Three Sisters".to_string(), date: Date{ day: 15, month: 11, year: 2025 }, session_id: 2, race_position: 1 }, ..Default::default() },Race{ race_information: RaceInformation { track_name: "Three Sisters".to_string(), date: Date{ day: 15, month: 11, year: 2025 }, session_id: 3, race_position: 1 }, ..Default::default() }, true)]
-    #[case(Race{ race_information: RaceInformation { track_name: "Three Sisters".to_string(), date: Date{ day: 15, month: 11, year: 2025 }, session_id: 1, race_position: 1 }, ..Default::default() },Race{ race_information: RaceInformation { track_name: "Four Sisters".to_string(), date: Date{ day: 15, month: 11, year: 2025 }, session_id: 1, race_position: 1 }, ..Default::default() },Race{ race_information: RaceInformation { track_name: "Five Sisters".to_string(), date: Date{ day: 15, month: 11, year: 2025 }, session_id: 1, race_position: 1 }, ..Default::default() }, true)]
-    fn is_unique_identifer(
-        #[case] race_1: Race,
-        #[case] race_2: Race,
-        #[case] race_3: Race,
-        #[case] expected_is_unique: bool,
-    ) {
-        // Given
-        let races = vec![race_2, race_3];
-
-        // When
-        let is_unique = race_1.is_unique_identifer(&races);
-
-        // Then
-        assert_eq!(expected_is_unique, is_unique)
-    }
-
     #[test]
-    fn replace_existing_race() {
-        // Given
-        let updated_race = Race {
-            race_information: RaceInformation {
-                track_name: "Trafford Park".to_string(),
-                date: Date {
-                    day: 15,
-                    month: 11,
-                    year: 2025,
-                },
-                session_id: 1,
-                race_position: 5,
-            },
-            laptimes: vec![Lap {
-                lap_number: 1,
-                time: 10.5,
-            }],
-        };
-
-        let existing_races = vec![
-            Race {
-                race_information: RaceInformation {
-                    track_name: "Trafford Park".to_string(),
-                    date: Date {
-                        day: 15,
-                        month: 11,
-                        year: 2025,
-                    },
-                    session_id: 1,
-                    race_position: 3,
-                },
-                laptimes: vec![Lap {
-                    lap_number: 1,
-                    time: 9.5,
-                }],
-            },
-            Race {
-                race_information: RaceInformation {
-                    track_name: "Trafford Park".to_string(),
-                    date: Date {
-                        day: 15,
-                        month: 11,
-                        year: 2025,
-                    },
-                    session_id: 2,
-                    race_position: 5,
-                },
-                laptimes: vec![Lap {
-                    lap_number: 1,
-                    time: 10.5,
-                }],
-            },
-        ];
-
-        let expected_updated_existing_races = vec![
-            Race {
-                race_information: RaceInformation {
-                    track_name: "Trafford Park".to_string(),
-                    date: Date {
-                        day: 15,
-                        month: 11,
-                        year: 2025,
-                    },
-                    session_id: 1,
-                    race_position: 5,
-                },
-                laptimes: vec![Lap {
-                    lap_number: 1,
-                    time: 10.5,
-                }],
-            },
-            Race {
-                race_information: RaceInformation {
-                    track_name: "Trafford Park".to_string(),
-                    date: Date {
-                        day: 15,
-                        month: 11,
-                        year: 2025,
-                    },
-                    session_id: 2,
-                    race_position: 5,
-                },
-                laptimes: vec![Lap {
-                    lap_number: 1,
-                    time: 10.5,
-                }],
-            },
-        ];
-
-        // When
-        let updated_existing_races = updated_race.replace_existing_race(&existing_races);
-
-        // Then
-        assert_eq!(expected_updated_existing_races, updated_existing_races)
-    }
+    #[ignore]
+    fn convert_laps_to_laptimes() {}
 }
