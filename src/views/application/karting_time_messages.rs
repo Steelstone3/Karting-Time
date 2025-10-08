d9c016d691dd062133c3af22b04fc797808bc1ce
Skippy <steelstone3@gmail.com>
2025-10-08 04:49:21 +0100

    version-08-10-25

--- src/views/application/karting_time_messages.rs
+++ src/views/application/karting_time_messages.rs
@@ -1,3 +1,5 @@
+use iced::Task;
+
 use crate::{
     commands::messages::Message,
     controllers::file::file_picker::{
@@ -7,46 +9,64 @@ use crate::{
 };
 
 impl KartingTime {
-    pub fn update(&mut self, message: Message) {
+    pub fn update(&mut self, message: Message) -> Task<Message> {
         match message {
-            Message::MenuBar => {}
-            Message::SelectedTabChanged(tab_identifier) => self.switch_tab(tab_identifier),
-            Message::FileNew => self.file_new(),
-            Message::ImportRacesRequested => {
-                self.import_races(select_files_to_load());
-                self.driver_profile.sort_races();
-                self.driver_profile.update_filtering();
+            Message::MenuBar => Task::none(),
+            Message::SelectedTabChanged(tab_identifier) => {
+                self.switch_tab(tab_identifier);
+                Task::none()
             }
-            Message::ImportRacesCompleted(file_paths) => match file_paths {
-                Some(file_paths) => {
+            Message::FileNew => {
+                self.file_new();
+                Task::none()
+            }
+            Message::ImportRacesRequested => {
+                select_files_to_load().map(Message::ImportRacesCompleted)
+            }
+            Message::ImportRacesCompleted(file_paths) => {
+                if let Some(file_paths) = file_paths {
                     self.import_races(file_paths);
                     self.driver_profile.sort_races();
                     self.driver_profile.update_filtering();
                 }
-                None => {}
-            },
-            Message::ExportRaces => self.export_races(&save_folder_location()),
-            Message::ExportHtmlRaces => self.export_html_races(&save_folder_location()),
-            Message::SaveApplication => self.save_application(&save_file_location()),
-            Message::LoadApplicationRequested => {
-                self.load_application(&select_file_to_load());
-                self.driver_profile.sort_races();
-                self.driver_profile.update_filtering();
+                Task::none()
             }
-            Message::LoadApplicationCompleted(file_path) => match file_path {
-                Some(file_path) => {
+            Message::ExportRaces => {
+                self.export_races(&save_folder_location());
+                Task::none()
+            }
+            Message::ExportHtmlRaces => {
+                self.export_html_races(&save_folder_location());
+                Task::none()
+            }
+            Message::SaveApplication => {
+                self.save_application(&save_file_location());
+                Task::none()
+            }
+            Message::LoadApplicationRequested => {
+                select_file_to_load().map(Message::LoadApplicationCompleted)
+            }
+            Message::LoadApplicationCompleted(file_path) => {
+                if let Some(file_path) = file_path {
                     self.load_application(&file_path);
                     self.driver_profile.sort_races();
                     self.driver_profile.update_filtering();
                 }
-                None => {}
-            },
-            Message::ViewToggleTheme => self.switch_theme(),
+                Task::none()
+            }
+            Message::ViewToggleTheme => {
+                self.switch_theme();
+                Task::none()
+            }
             Message::ViewToggleFilter => {
                 self.toggle_filter();
                 self.driver_profile.update_filtering();
+                Task::none()
             }
-            Message::DriverNameChanged(name) => self.driver_profile.name = name,
+            Message::DriverNameChanged(name) => {
+                self.driver_profile.name = name;
+                Task::none()
+            }
             Message::TrackNameChanged(track_name) => {
                 self.driver_profile.new_race.race_information.track_name = track_name;
 
@@ -54,6 +74,7 @@ impl KartingTime {
                     .new_race
                     .race_information
                     .update_unique_identifier();
+                Task::none()
             }
             Message::DayChanged(day) => {
                 self.driver_profile
@@ -66,6 +87,7 @@ impl KartingTime {
                     .new_race
                     .race_information
                     .update_unique_identifier();
+                Task::none()
             }
             Message::MonthChanged(month) => {
                 self.driver_profile
@@ -78,6 +100,7 @@ impl KartingTime {
                     .new_race
                     .race_information
                     .update_unique_identifier();
+                Task::none()
             }
             Message::YearChanged(year) => {
                 self.driver_profile
@@ -90,6 +113,7 @@ impl KartingTime {
                     .new_race
                     .race_information
                     .update_unique_identifier();
+                Task::none()
             }
             Message::SessionIdChanged(session_id) => {
                 self.driver_profile
@@ -102,12 +126,15 @@ impl KartingTime {
                     .new_race
                     .race_information
                     .update_unique_identifier();
+                Task::none()
             }
             Message::SessionTypeChanged(session_type) => {
                 self.driver_profile.new_race.race_metadata.session_type = session_type;
+                Task::none()
             }
             Message::TrackConditionsChanged(track_conditions) => {
                 self.driver_profile.new_race.race_metadata.track_conditions = track_conditions;
+                Task::none()
             }
             Message::RacePositionChanged(race_position) => {
                 self.driver_profile
@@ -115,45 +142,56 @@ impl KartingTime {
                     .race_information
                     .session
                     .set_race_position(race_position);
+                Task::none()
             }
             Message::CarUsedChanged(car_used) => {
                 self.driver_profile.new_race.race_metadata.car_used = car_used;
+                Task::none()
             }
             Message::ChampionshipChanged(championship) => {
                 self.driver_profile.new_race.race_metadata.championship = championship;
+                Task::none()
             }
             Message::NotesChanged(notes) => {
                 self.driver_profile.new_race.race_metadata.notes = notes;
+                Task::none()
             }
-            Message::LaptimeEditor(action) => self
-                .application_state
-                .race_editor
-                .text_editor
-                .perform(action),
+            Message::LaptimeEditor(action) => {
+                self.application_state
+                    .race_editor
+                    .text_editor
+                    .perform(action);
+                Task::none()
+            }
             Message::TrackFilterChanged(track_query) => {
                 self.driver_profile.filter.track_query = track_query;
 
                 self.driver_profile.update_filtering();
+                Task::none()
             }
             Message::DateFilterChanged(date_query) => {
                 self.driver_profile.filter.date_query = date_query;
 
                 self.driver_profile.update_filtering();
+                Task::none()
             }
             Message::CarUsedFilterChanged(car_used_query) => {
                 self.driver_profile.filter.car_used_query = car_used_query;
 
                 self.driver_profile.update_filtering();
+                Task::none()
             }
             Message::ChampionshipFilterChanged(championship_query) => {
                 self.driver_profile.filter.championship_query = championship_query;
 
                 self.driver_profile.update_filtering();
+                Task::none()
             }
             Message::SessionTypeFilterChanged(session_type_query) => {
                 self.driver_profile.filter.session_type_query = session_type_query;
 
                 self.driver_profile.update_filtering();
+                Task::none()
             }
             Message::UpdateRacesPressed => {
                 self.driver_profile.new_race.convert_to_laps(
@@ -165,9 +203,11 @@ impl KartingTime {
                 self.driver_profile.upsert_race();
                 self.driver_profile.sort_races();
                 self.driver_profile.update_filtering();
+                Task::none()
             }
             Message::ClearRaceEditorPressed => {
                 self.application_state.race_editor.clear_text_editor();
+                Task::none()
             }
             Message::ReplacePressed(identifier) => {
                 if let Some(race) = self
@@ -181,6 +221,7 @@ impl KartingTime {
                     self.application_state.race_editor.paste_laptimes(race);
                     self.driver_profile.update_filtering();
                 }
+                Task::none()
             }
             Message::DeletePressed(identifier) => {
                 if let Some(race) = self
@@ -192,6 +233,7 @@ impl KartingTime {
                     race.is_deleting = true;
                     self.driver_profile.update_filtering();
                 }
+                Task::none()
             }
             Message::DeleteConfirmedPressed(identifier) => {
                 if let Some(index) = self
@@ -203,6 +245,7 @@ impl KartingTime {
                     self.driver_profile.races.remove(index);
                     self.driver_profile.update_filtering();
                 }
+                Task::none()
             }
             Message::DeleteCancelledPressed(identifier) => {
                 if let Some(race) = self
@@ -214,6 +257,7 @@ impl KartingTime {
                     race.is_deleting = false;
                     self.driver_profile.update_filtering();
                 }
+                Task::none()
             }
         }
     }
