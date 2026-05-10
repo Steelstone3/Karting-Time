8c8f59103977f3830de00336287a549ebdc01264
Skippy <steelstone3@gmail.com>
2026-05-10 21:01:35 +0100

    version-10-05-26

--- src/models/driver/session_information/acc_lap.rs
+++ src/models/driver/session_information/acc_lap.rs
@@ -8,3 +8,12 @@ pub struct AccLap {
     #[serde(rename = "laptime")]
     pub laptime: f32,
 }
+
+impl AccLap {
+    pub fn new(driver_index: u32, laptime: f32) -> Self {
+        Self {
+            driver_index,
+            laptime,
+        }
+    }
+}
--- src/models/driver/session_information/acc_session_data.rs
+++ src/models/driver/session_information/acc_session_data.rs
@@ -14,12 +14,48 @@ pub struct AccSessionData {
 }
 
 impl AccSessionData {
-    pub fn convert_to_laptimes(&self) -> Vec<String> {
+    // pub fn new(track_name: &str, session_type: &str, laps: Vec<AccLap>) -> Self {
+    //     Self {
+    //         track_name: track_name.to_string(),
+    //         session_type: session_type.to_string(),
+    //         session_index: 999,
+    //         laps,
+    //     }
+    // }
+
+    // // TODO test
+    // pub fn extract_driver_laptimes(&self) -> Vec<AccSessionData> {
+    //     vec![]
+    // }
+
+    // TODO test
+    pub fn convert_to_laptimes(&self) -> Vec<Vec<String>> {
         self.laps
             .clone()
             .into_iter()
             // Convert from milliseconds
             .map(|lap| (lap.laptime / 1000.0).to_string())
             .collect()
+    }
+}
+
+#[cfg(test)]
+mod acc_session_data_should {
+    use super::*;
+
+    #[test]
+    fn convert_to_laptimes_test() {
+        // Given
+        let acc_laps = vec![AccLap::new(0, 32.2)];
+        let acc_session_data = AccSessionData {
+            laps: acc_laps,
+            ..Default::default()
+        };
+
+        // When
+        let acc_laps_by_driver = acc_session_data.convert_to_laptimes();
+
+        // Then
+        pretty_assertions::assert_eq!()
     }
 }
